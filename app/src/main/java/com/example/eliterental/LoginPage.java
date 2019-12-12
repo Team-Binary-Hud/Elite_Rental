b3b4c6b4e2f9865cd6c0f8d4142cd2452489de20
Stephen Roberts <stephen.roberts14@sky.com>
2019-12-12 10:05:50 +0000

    c

---
+++ app/src/main/java/com/example/eliterental/LoginPage.java
@@ -0,0 +1,34 @@
+package com.example.eliterental;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+    CharSequence UsernameExample = "Name123";
+    CharSequence PasswordExample = "password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void checkLoginDetails(View view) {
+        TextView logintext = findViewById(R.id.LoginText);
+        TextView username = findViewById(R.id.usernametext);
+        TextView password = findViewById(R.id.passwordtext);
+        if(username.getText() == UsernameExample){
+            if(password.getText() == PasswordExample){
+                logintext.setText("Login Correct");
+            } else {
+                logintext.setText("Login Incorrect");
+            }
+        } else {
+            //logintext.setText("Login Incorrect");
+            logintext.setText(username.getText());
+        }
+    }
+}
