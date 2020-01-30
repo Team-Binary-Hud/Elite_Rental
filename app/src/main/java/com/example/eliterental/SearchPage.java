559ddf6307c58c3a7d58905991e7fa99a6158b35
Stephen Roberts <stephen.roberts14@sky.com>
2020-01-30 10:52:29 +0000

    23/01/20 10:46

--- app/src/main/java/com/example/eliterental/SearchPage.java
+++ app/src/main/java/com/example/eliterental/SearchPage.java
@@ -3,8 +3,21 @@ package com.example.eliterental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewStub;
+import android.widget.ListView;
 
 public class SearchPage extends AppCompatActivity {
+
+    public static Integer CarRentalID;
+    public static String CarModel;
+    public static String CarYear;
+    public static String CarColour;
+    public static String RentalPrice;
+
+    private ViewStub car_list_stub;
+    private ListView CarListView;
+    //private List<Rentals> RentalList; TEMP LIST OF EXAMPLE CARS
+    //getRentalList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
