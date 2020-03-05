5e9e9a031ce2c57199cc92267879b265fc0f9559
L.Saleem <luqmaansaleem00@gmail.com>
2020-03-05 11:03:47 +0000

    luqmaan commit 05/03

--- app/src/main/java/com/example/eliterental/RegisterPage.java
+++ app/src/main/java/com/example/eliterental/RegisterPage.java
@@ -6,32 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterPage extends AppCompatActivity {
-    private EditText Name;
-    private EditText Email;
-    private EditText Password;
-    private EditText RePassword;
-    private Button RegisterButton;
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private EditText rePassword;
+    private EditText phoneNumber;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-        Name = findViewById(R.id.RegisterName);
-        Email = findViewById(R.id.RegisterEmail);
-        Password = findViewById(R.id.RegisterPassword);
-        RePassword = findViewById(R.id.RegisterRePassword);
-        RegisterButton = findViewById(R.id.RegistrationButton);
+        name = findViewById(R.id.RegisterName);
+        email = findViewById(R.id.RegisterEmail);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        password = findViewById(R.id.RegisterPassword);
+        rePassword = findViewById(R.id.RegisterRePassword);
+        registerButton = findViewById(R.id.RegistrationButton);
 
 
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
@@ -49,8 +50,8 @@ public class RegisterPage extends AppCompatActivity {
         boolean lowerCaseFlag = false;
         boolean numberFlag = false;
         boolean specialChar = false;
-        for (int i = 0; i < Password.getText().length(); i++) {
-            ch = Password.getText().charAt(i);
+        for (int i = 0; i < password.getText().length(); i++) {
+            ch = password.getText().charAt(i);
             if (Character.isDigit(ch)) {
                 numberFlag = true;
             } else if (Character.isUpperCase(ch)) {
@@ -69,8 +70,9 @@ public class RegisterPage extends AppCompatActivity {
     private boolean emailStringCheck() {
         boolean email = false;
 
-        String emailText = Email.getText().toString();
-        if (!emailText.startsWith("@") || !emailText.endsWith("@") || emailText.contains("@") || emailText.length() > 6 || emailText.endsWith(".com") || emailText.endsWith(".co.uk")) {
+        String emailText = this.email.getText().toString();
+        if (!emailText.isEmpty() && !emailText.startsWith("@") && !emailText.endsWith("@") && emailText.contains("@") &&
+                emailText.length() > 6 && emailText.endsWith("gmail.com") || emailText.endsWith(".co.uk")) {
             email = true;
         }
 
@@ -78,33 +80,69 @@ public class RegisterPage extends AppCompatActivity {
         else return false;
     }
 
-    private boolean nameChecker() {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private boolean nameStringCheck() {
         boolean name = true;
         char ch;
-        String nameText = Name.getText().toString();
+        int whiteSpaces = 0;
 
-        for (int i = 0; i < Password.getText().length(); i++) {
-            ch = Password.getText().charAt(i);
+        for (int i = 0; i < this.name.getText().length(); i++) {
+            ch = this.name.getText().charAt(i);
             if (Character.isDigit(ch))
                 name = false;
+            else if (!Character.isAlphabetic(ch) && !Character.isDigit(ch) && !Character.isWhitespace(ch))
+                name = false;
+            else if (Character.isWhitespace(ch))
+                whiteSpaces++;
         }
-        return name;
+        if (!name || whiteSpaces > 1) return false;
+        else return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private boolean phoneStringCheck() {
+        boolean phoneN = true;
+        char ch;
+        int phoneNumLength = 0;
+
+        for (int i = 0; i < this.phoneNumber.getText().length(); i++) {
+            ch = this.phoneNumber.getText().charAt(i);
+            if (Character.isAlphabetic(ch))
+                phoneN = false;
+            else if (!Character.isAlphabetic(ch) && !Character.isDigit(ch) && !Character.isWhitespace(ch))
+                phoneN = false;
+            else if (Character.isWhitespace(ch))
+                phoneN = false;
+            else if (Character.isDigit(ch))
+                phoneNumLength++;
+        }
+        if (!phoneN || phoneNumLength != 10) return false;
+        else return true;
+    }
+
+    private boolean postCodeCheck() {
+        boolean postCode = true;
+        char ch;
+
+        return false;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void credentialChecker() {
 
         Intent intentLogin = new Intent(this, LoginPage.class);
-        if (!(Password.getText().toString().equals(RePassword.getText().toString())))
-            Toast.makeText(this, "Password did not match", Toast.LENGTH_SHORT).show();
-        else if (Password.getText().toString().length() < 8) {
+        if (!nameStringCheck() || name.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Not a valid name", Toast.LENGTH_SHORT).show();
+        } else if (!emailStringCheck()) {
+            Toast.makeText(this, "Not a valid email", Toast.LENGTH_SHORT).show();
+        } else if (!phoneStringCheck() || phoneNumber.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Not a valid Number", Toast.LENGTH_SHORT).show();
+        } else if (password.getText().toString().length() < 8) {
             Toast.makeText(this, "Password needs to be at least 8 characters", Toast.LENGTH_SHORT).show();
         } else if (!passwordStringCheck()) {
-            Toast.makeText(this, "Password needs at least:\nOne Uppercase:One Lowercase:\nOne Special Character:One Number", Toast.LENGTH_LONG).show();
-        } else if (!emailStringCheck()) {
-            Toast.makeText(this, "Not a valid Email", Toast.LENGTH_SHORT).show();
-//        } else if () {
-
+            Toast.makeText(this, "password needs at least:\nOne Uppercase:One Lowercase:\nOne Special Character:One Number", Toast.LENGTH_LONG).show();
+        } else if (!(password.getText().toString().equals(rePassword.getText().toString()))) {
+            Toast.makeText(this, "Password entered did not match", Toast.LENGTH_SHORT).show();
         } else startActivity(intentLogin);
     }
 }
